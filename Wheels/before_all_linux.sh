6f160b9d22e6fc8aabb07e4c6ffb9c1f4f3446b6
Farah Khashman <farah.khashman@acadiasoft.inc>
2024-04-21 20:02:48 -0400

    second muslinux trial

--- Wheels/before_all_linux.sh
+++ Wheels/before_all_linux.sh
@@ -35,6 +35,6 @@ tar xfz boost_1_80_0.tar.gz
 cd boost_1_80_0
 export Eigen3_DIR=$CURRENT_DIR/eigen-3.4.0
 ./bootstrap.sh --with-libraries=date_time,filesystem,iostreams,log,regex,serialization,system,thread,timer
-./b2 cxxflags=-fPIC cxxflags=-fvisibility=default install -sZLIB_SOURCE=$CURRENT_DIR/zlib-1.3.1
+./b2 install -sZLIB_SOURCE=$CURRENT_DIR/zlib-1.3.1
 cd ..
 echo "XYZ END unpack boost"
